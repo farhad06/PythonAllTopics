0420c4a060b8db98639c3e9b63dc4f84750b33a4
Farhad Ahamed <farhadahamed06@gmail.com>
2022-06-23 20:54:05 +0530

    set commit

---
+++ set.py
@@ -0,0 +1,11 @@
+'''Set is also an another type of datatype in python which is store mutiple item in a single variable
+Set is:-----:
+1.Unordered
+2.unchangeable
+3.Do not allow duplicate
+'''
+#create anf print
+s={1,2,3,4,5}
+print(s)
+s1=set((3,4,5,67,25,45))
+print(s1)
